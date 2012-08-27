492bc9a23567f68ccbecb3bc87a647eaf967bc32
Luke.Miller.DRSS <Jernik@users.noreply.github.com>
2012-08-27 17:06:18 +0000

    Testing Netbeans 7.2 commit and also starting a Stem class that will form the basis of the new music system.

---
+++ Music/Stem.java
@@ -0,0 +1,11 @@
+package Music;
+
+/**
+ * This is a wrapper class for a Clip. It contains information about the clip, such as 
+ * genre, type of clip (percussion, mid, bass, treble, etc) as well as the location of the
+ * file and any other information necessary to playing/mixing the clip.
+ * @author Luke M
+ */
+public class Stem {
+    
+}
