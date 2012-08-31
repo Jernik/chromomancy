4910e42b05f095ce0603fddf64ab50bb9fd9dea9
brendan928@gmail.com <brendan928@gmail.com@64e4784e-1cd9-92cc-1d0a-df9851b6be1e>
2012-08-31 18:06:56 +0000

    AddedCodeToMap8

--- trunk/Maps/Map8.java
+++ trunk/Maps/Map8.java
@@ -6,4 +6,24 @@ package Maps;
  */
 public class Map8 extends Map{
     
+	public Map8() {}
+	/**
+	* {inheritdoc}
+	*/
+	public void init() {
+		levelName="Map8";
+		ConcreteObject.Entity flowerKing=new Maps.Entities.FlowerKing8();
+		flowerKing.Color(127,127,127);
+		flowerKing.Displacement(400,400);
+		flowerKing.D=this.D;
+		this.entities.add(flowerKing);
+		((Maps.Entities.FlowerKing8)flowerKing).init();
+	}
+	/**
+	* {inheritdoc}
+	*/
+	public void draw(java.awt.image.DataBuffer buffer) {
+		drawConnections(buffer);
+	}
+	
 }
